9f912eaacbceaf014a1b161bb2b6705e572eef1a
Ahmadkashif <ahmadkashif983@gmail.com>
2020-09-17 02:02:10 +0500

    initial commit

---
+++ attempt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 17 01:56:53 2020
+
+@author: ahmad
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
